37a655b692a764b3c6decb8a151d149fe37e63a6
Bo Thompson <AimlessZealot@gmail.com>
2021-08-05 14:03:51 -0400

    First Commit, Yo!

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UDPSocket c = new UDPSocket();
+            c.Client("162.255.85.127", 7777);
+            c.Send("TEST!");
+
+            Console.ReadKey();
+        }
+    }
+}
